c41f4069b88fa5571116323134ff93ec9a805886
Willie169 <cg10250207@gmail.com>
2025-02-02 00:00:43 +0800

    m

--- md2html.js
+++ md2html.js
@@ -1,9 +1,14 @@
 // npm install marked
 
 const fs = require('fs');
-const marked = require('marked');
+const { marked } = require('marked');
 
-const markdown = fs.readFileSync('README.md', 'utf8');
-const htmlContent = marked(markdown);
-
-fs.writeFileSync('index.html', htmlContent);
+try {
+  const markdown = fs.readFileSync('README.md', 'utf8');
+  const htmlContent = marked(markdown);
+  
+  fs.writeFileSync('index.html', htmlContent);
+  console.log('HTML file has been saved as index.html');
+} catch (err) {
+  console.error('An error occurred:', err);
+}
